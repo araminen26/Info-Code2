5fac2b4c66a7e7655a25c2427d7468385c279491
Sruthi Ramineni <sruthiramineni@Sruthis-MacBook-Pro-2.local>
2024-03-24 09:21:38 -0400

    initial commit

---
+++ src/test/java/com/code/InfoCode/InfoCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.InfoCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
